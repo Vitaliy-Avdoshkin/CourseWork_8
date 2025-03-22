a2316d2a49368c7604634d76c59f05c5d39c9d3a
AKVA <wioswa@gmail.com>
2025-03-23 00:45:48 +0300

    CW_9 || Deploy

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['51.250.101.133', 'django', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['http://51.250.101.133']
 
 
 INSTALLED_APPS = [
